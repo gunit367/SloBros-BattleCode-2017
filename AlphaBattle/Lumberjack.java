3d381e24ea98aaee551aba1ca434442e264fa801
Tristan Broughton <Tristan@tristans-mbp.home>
2017-01-11 16:22:07 -0800

    began building soldier class

--- AlphaBattle/Lumberjack.java
+++ AlphaBattle/Lumberjack.java
@@ -2,4 +2,6 @@ package AlphaBattle;
 
 public class Lumberjack {
 
+	
+	
 }
--- AlphaBattle/Soldier.java
+++ AlphaBattle/Soldier.java
@@ -1,5 +1,52 @@
 package AlphaBattle;
+import battlecode.common.*;
 
 public class Soldier {
+	static RobotController rc;
+	public Soldier()
+	{
+		
+	}
+	
+	public void run()
+	{
+		//helper functions: isEnemyClose(), getEnemyLocation(), shoot(location), 
+		System.out.println("I'm an soldier!");
+	    Team enemy = rc.getTeam().opponent();
 
+	    // The code you want your robot to perform every round should be in this loop
+	    while (true) {
+
+	        // Try/catch blocks stop unhandled exceptions, which cause your robot to explode
+	        try {
+	            //MapLocation myLocation = rc.getLocation();
+
+	            // See if there are any nearby enemy robots
+	            RobotInfo[] robots = rc.senseNearbyRobots(-1, enemy);
+
+	            // If there are some...
+	            if (robots.length > 0) {
+	                // And we have enough bullets, and haven't attacked yet this turn...
+	                if (rc.canFireSingleShot()) {
+	                    // ...Then fire a bullet in the direction of the enemy.
+	                    rc.fireSingleShot(rc.getLocation().directionTo(robots[0].location));
+	                }
+	            }
+
+	            // Clock.yield() makes the robot wait until the next turn, then it will perform this loop again
+	            Clock.yield();
+	            
+	            //Move
+	            //use Grant's 'util' class
+
+	        } catch (Exception e) {
+	            System.out.println("Soldier Exception");
+	            e.printStackTrace();
+	        }
+	    }
+	}
+	
+	
+	
+	
 }
