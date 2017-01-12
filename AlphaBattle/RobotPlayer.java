7882eb1e4138313e7378d71b45c8943759c96880
Michael McAniff <michael.mcaniff@gmail.com>
2017-01-11 16:21:32 -0800

    Commit merge fixes

--- AlphaBattle/RobotPlayer.java
+++ AlphaBattle/RobotPlayer.java
@@ -77,43 +77,8 @@ public strictfp class RobotPlayer {
     }
 
 	static void runGardener() throws GameActionException {
-<<<<<<< HEAD
        Gardener gardener = new Gardener();
        gardener.run();
-=======
-        System.out.println("I'm a gardener!");
-
-        // The code you want your robot to perform every round should be in this loop
-        while (true) {
-
-            // Try/catch blocks stop unhandled exceptions, which cause your robot to explode
-            try {
-
-            	// Get Random Direction
-            	Direction dir = Util.randomDirection();
-            	
-                // Listen for home archon's location
-                int xPos = rc.readBroadcast(0);
-                int yPos = rc.readBroadcast(1);
-                MapLocation archonLoc = new MapLocation(xPos,yPos);
-
-                //Attempt to build a lumberjack in this direction
-                if (rc.canBuildRobot(RobotType.LUMBERJACK, dir) && rc.isBuildReady()) {
-                    rc.buildRobot(RobotType.LUMBERJACK, dir);
-                }
-
-                // Move away from archon
-                tryMove(archonLoc.directionTo(rc.getLocation()));
-
-                // Clock.yield() makes the robot wait until the next turn, then it will perform this loop again
-                Clock.yield();
-
-            } catch (Exception e) {
-                System.out.println("Gardener Exception");
-                e.printStackTrace();
-            }
-        }
->>>>>>> f2269dde64189c052ef16f39844c8237c6d8cefb
     }
 
     static void runSoldier() throws GameActionException {
@@ -199,7 +164,6 @@ public strictfp class RobotPlayer {
         }
     }
 
-<<<<<<< HEAD
     /**
      * Returns a random Direction
      * @return a random Direction
@@ -207,8 +171,7 @@ public strictfp class RobotPlayer {
     public static Direction randomDirection() {
         return new Direction((float)Math.random() * 2 * (float)Math.PI);
     }
-=======
->>>>>>> f2269dde64189c052ef16f39844c8237c6d8cefb
+
 
     /**
      * Attempts to move in a given direction, while avoiding small obstacles directly in the path.
